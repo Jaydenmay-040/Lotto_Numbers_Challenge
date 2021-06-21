3b66c3a288cc6706181be17641894a4d7eda2551
Jaydenmay-040 <82215016+Jaydenmay-040@users.noreply.github.com>
2021-06-21 19:57:17 +0200

    Update Login_user_testing.py

--- Login_user_testing.py
+++ Login_user_testing.py
@@ -1,1 +1,16 @@
+import unittest
+from Login_user import lotto
 import random
+import rsaidnumber
+from dateutil import relativedelta
+from datetime import datetime
+
+
+class Testing Lotto(unittest.TestCase):
+    def test_compare(self):
+        instance = lotto()
+# instantiation
+        a = [1, 2, 2]
+        b = [1, 2, 3]
+        self.assertEqual(2, instance.compare(a,b))
+
