538fafc8e46388ae47268c2159f76a5682deb561
Jaydenmay-040 <82215016+Jaydenmay-040@users.noreply.github.com>
2021-06-21 20:11:07 +0200

    Update Login_user_testing.py

--- Login_user_testing.py
+++ Login_user_testing.py
@@ -13,4 +13,20 @@ class Testing Lotto(unittest.TestCase):
         a = [1, 2, 2]
         b = [1, 2, 3]
         self.assertEqual(2, instance.compare(a,b))
+    
+    def test_random(self):
+        rand_num = random.randint(1, 49)
+        self.assertTrue(0 < rand_num < 50, "Random value not in range 1-49")
 
+    def test_rsa_id(self):
+        id_pass = rsaidnumber.parse("0211015147088")
+        self.assertTrue(id_pass, "ID should return true")
+
+    def test_age(self):
+        id_number = rsaidnumber.parse("0211015147088")
+        age = relativedelta.relativedelta(datetime.today(), id_number.date_of_birth).years
+        self.assertTrue(age >= 18, "Should return true")
+
+
+if __name__ == '__Login_user__':
+    unittest.Login_user()
